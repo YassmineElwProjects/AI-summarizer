023f63f3ba2e89dbb0a523a640432ff3e6bfb0a0
YassmineelwProjects <yassmelw@gmail.com>
2023-09-19 21:30:48 -0400

    Fetch and show article summary for the specified url

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,32 +2,43 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 import { data } from "autoprefixer";
- 
+
 const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
-
-
+  const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-  const handleSubmit = async(e) => {
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const {data} = await getSummary({articleUrl: article.url});
-  
-  if(data?.summary){ 
-    const newArticle = {
-      ...article, summary: data.summary
-    };
-    setArticle(newArticle);
-    console.log(newArticle); 
-  }
-  }
-  
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = {
+        ...article,
+        summary: data.summary,
+      };
+      const updatedAllArticles = [newArticle, ...allArticles];
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search*/}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={ handleSubmit }
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -38,7 +49,9 @@ const Demo = () => {
             type="url"
             placeholder="Enter a URL"
             value={article.url}
-            onChange= {(e) => {setArticle({...article, url: e.target.value})}}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
             required
             className="url_input peer"
           />
@@ -50,8 +63,53 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="Copy-icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Display result*/}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            It seems there is an error, please try again later.
+            <br />
+            <span className="font-satoshi text-gray-700 font-normal">
+              {error?.data?.message}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div>
+              <p className="font-inter font-medium text-sm text-gray-600">{article.summary}</p>
+              </div>
+
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
